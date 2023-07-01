cdb3e45ca6099f253740e1efd5de7c779b07d172
0xbigshaq <57250448+0xbigshaq@users.noreply.github.com>
2023-07-01 14:07:52 +0300

    fix debug container setup

---
+++ .gdbinit
@@ -0,0 +1,2 @@
+file /share/build/kernel.elf
+gef-remote --qemu-user --qemu-binary /share/build/kernel.elf localhost 1234
--- deps-container.sh
+++ deps-container.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 apt update
-apt install \
+DEBIAN_FRONTEND=noninteractive apt install \
     build-essential \
     gdb \
     file \
@@ -11,9 +11,17 @@ apt install \
     nasm \
     git \
     sudo \
-    grub-mkrescue \
+    grub-common \
+    grub-pc \
+    xorriso \
     -y
 
+git clone --branch main https://github.com/hugsy/gef.git /root/gef/
+echo "source /root/gef/gef.py" > ~/.gdbinit 
+echo "add-auto-load-safe-path /share/.gdbinit" >> ~/.gdbinit
+
+echo "export LC_CTYPE=C.UTF-8" >> ~/.bashrc
+echo "export PS1='\[\e[1;34m\]\u@\h \[\e[1;32m\]\w \[\e[1;31m\]🌀 \\$ \[\e[0m\]'" >> ~/.bashrc
 
 # Note: to exit qemu, Ctrl-A X    (source: https://www.ics.uci.edu/~aburtsev/238P/2020spring/hw/hw2-boot-xv6.html)
 
