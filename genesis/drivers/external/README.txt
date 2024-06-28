28d28ba942d121dad15b4c1dc6efa9a2550df987
0xbigshaq <57250448+0xbigshaq@users.noreply.github.com>
2024-06-28 17:26:18 +0300

    fs: integrate fatfs into genesis CMake flow

--- genesis/drivers/external/README.txt
+++
@@ -1,2 +0,0 @@
-
-External libs/helpers for the kernel drivers
--- genesis/drivers/ff15/diskio.c
+++ genesis/drivers/ff15/diskio.c
@@ -1,11 +1,6 @@
 /*-----------------------------------------------------------------------*/
 /* Low level disk I/O module SKELETON for FatFs     (C)ChaN, 2019        */
 /*-----------------------------------------------------------------------*/
-/* If a working storage control module is available, it should be        */
-/* attached to the FatFs via a glue function rather than modifying it.   */
-/* This is an example of glue functions to attach various exsisting      */
-/* storage control modules to the FatFs module with a defined API.       */
-/*-----------------------------------------------------------------------*/
 
 #include "ff.h"			/* Obtains integer types */
 #include "diskio.h"		/* Declarations of disk functions */
