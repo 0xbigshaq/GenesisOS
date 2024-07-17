ea5f437c55728617c70eebebb8ec65788ee86321
0xbigshaq <57250448+0xbigshaq@users.noreply.github.com>
2024-07-17 23:22:44 +0300

    debug: save logs if qemu crash

--- meta/dbg.sh
+++ meta/dbg.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
 
 
-qemu-system-i386 -s -S -nographic -boot d -cdrom build/kernel.iso -hda build/disk.img
+
+# qemu-system-i386 \
+#     -s -S \
+#     -boot d \
+#     -cdrom build/kernel.iso \
+#     -hda build/disk.img \
+#     -vga std -serial mon:stdio \
+#     -m 512
+
+qemu-system-i386 \
+    -s -S \
+    -no-reboot \
+    -boot d \
+    -cdrom build/kernel.iso \
+    -d cpu_reset,guest_errors,int,pcall,trace:all -D qemu_debug.log \
+    -hda build/disk.img \
+    -vga std -serial mon:stdio \
+    -m 512
